48f1c4190345802548132a5fb04402297cfe9ebc
Wookh-ing <104603755+Wookh-ing@users.noreply.github.com>
2022-06-07 23:44:42 +0900

    Delete WookEc/220603 -01 Date/src/220607 -01 package directory

--- WookEc/220603 -01 Date/src/220607 -01 package/src/first/second/AnotherClass.java
+++
@@ -1,9 +0,0 @@
-package first.second;
-
-public class AnotherClass {
-	public int number;
-	private int mySecret;
-	// int 만 있는건 default or package 접근제한자라 칭한다.
-	// default / package access modifier
-	int test;
-}
